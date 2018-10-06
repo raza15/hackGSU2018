6e57a6b3e81d01c4cee82f4389516a33cf0187dc
Raza Hussain <razahussain@Razas-MBP.gsu.edu>
2018-10-06 15:27:53 -0400

    created a new homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'sessions#new'
+  root 'sessions#gsu_new'
 
   get '/whichpayment', to: 'sessions#whichpayment'
   get '/creditpayment', to: 'sessions#creditpayment'
